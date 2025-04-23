c48ece79779b831d86e574f336ee7be548e039cc
william <william.dodart@gmail.com>
2025-04-23 16:02:14 +0200

    Testing of association genre/book model

--- src/migrations/seeding_tables.sql
+++ src/migrations/seeding_tables.sql
@@ -8,4 +8,26 @@ VALUES
     (2070518426, 'Harry Potter à l’école des sorciers', 'JK Rowling', 'Harry découvre quil est un sorcier blablabla...', 'https://m.media-amazon.com/images/I/71N6SgkNlcL._AC_UF1000,1000_QL80_.jpg', 305, 'Gallimard Jeunesse', 1998)
 ;
 
+INSERT INTO "genre"
+    ("name")
+VALUES
+    ('Horreur'),
+    ('Fantastique'),
+    ('Dystopique'),
+    ('Science-fiction'),
+    ('Fantasy'),
+    ('Jeunesse')
+;
+
+INSERT INTO "genre_book"
+    ("genre_id", "book_id")
+VALUES
+    (1,1),
+    (2,1),
+    (3,2),
+    (4,2),
+    (5,3),
+    (6,3)
+;
+
 COMMIT;
--- src/models/models.test.ts
+++ src/models/models.test.ts
@@ -1,5 +1,18 @@
-import { Book } from './association.model.js';
+import { Book, Genre } from './association.model.js';
 
 //test de récupération de tous les livres
-const allBooks = await Book.findAll();
-console.log(JSON.stringify(allBooks, null, 2));
+//const allBooks = await Book.findAll();
+//console.log(JSON.stringify(allBooks, null, 2));
+
+//test de récupération de tous les livres et de leurs genres
+const allBooksWithGenre = await Book.findAll({
+  include: {
+    model: Genre,
+    through: { attributes: [] },
+  },
+});
+console.log(JSON.stringify(allBooksWithGenre, null, 2));
+
+//test de récupération de tous les genres
+//const allGenres = await Genre.findAll();
+//console.log(JSON.stringify(allGenres, null, 2));
