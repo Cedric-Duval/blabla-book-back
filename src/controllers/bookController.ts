de1f71bc01db4b670d38e20781871d6d757d5eb8
titouan-pichon <titouan.pichon@oclock.school>
2025-04-24 11:04:21 +0200

    Add getAllBooks function to bookController.ts

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -4,17 +4,28 @@ import { Book } from '../models/association.model.js';
 export const bookController = {
   async getFiveRandomBooks(req, res) {
     try {
-      const allBooks = await Book.findAll({
+      const randomBooks = await Book.findAll({
         order: Sequelize.literal('RANDOM()'), //  Randomize order
         limit: 5
       });
-      console.log(JSON.stringify(allBooks, null, 2));
-      res.status(200).json(allBooks);
+      console.log(JSON.stringify(randomBooks, null, 2));
+      res.status(200).json(randomBooks);
     } catch (error) {
       console.error(error);
       res.status(500).json('Erreur interne du serveur');
     }
   },
+
+  async getAllBooks(req, res) {
+    try {
+      const allBooks = await Book.findAll();
+      console.log(JSON.stringify(allBooks, null, 2));
+      res.status(200).json(allBooks);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json('Erreur interne du serveur')
+    }
+  }
 
   async getOneBookById(req, res) {
     try {
