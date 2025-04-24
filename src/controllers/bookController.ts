8096e2e6f68c3b9e47f3c3b35fcecdf2f9db87c0
titouan-pichon <titouan.pichon@oclock.school>
2025-04-24 10:42:24 +0200

    Add randomize feature to getFiveRandomBooks in bookController.ts

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,9 +1,13 @@
+import { Sequelize } from 'sequelize';
 import { Book } from '../models/association.model.js';
 
 export const bookController = {
   async getFiveRandomBooks(req, res) {
     try {
-      const allBooks = await Book.findAll();
+      const allBooks = await Book.findAll({
+        order: Sequelize.literal('RANDOM()'), //  Randomize order
+        limit: 5
+      });
       console.log(JSON.stringify(allBooks, null, 2));
       res.status(200).json(allBooks);
     } catch (error) {
