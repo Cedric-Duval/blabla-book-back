4af7a4898d07d5c8814175e5f8423a4a2a7a56b7
titouan-pichon <titouan.pichon@oclock.school>
2025-04-24 10:52:23 +0200

    Add book/:id route to router.ts

--- src/router.ts
+++ src/router.ts
@@ -4,3 +4,5 @@ import { bookController } from './controllers/bookController.js';
 export const router = Router();
 
 router.get('/random-books', bookController.getFiveRandomBooks);
+
+router.get('/book/:id', bookController.getOneBookById); // Nodemon bugged with Regex expression
