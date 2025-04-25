55b08091da9a58fe75b5ac5921ba17ee7969c3ed
titouan-pichon <titouan.pichon@oclock.school>
2025-04-25 12:00:35 +0200

    Add libraryCreateSchema

--- src/schemas/library.schema.ts
+++ src/schemas/library.schema.ts
@@ -1,5 +1,9 @@
 import { z } from "zod";
 
+export const libraryCreateSchema = z.object({
+    name: z.string().max(100).nonempty()
+});
+
 export const libraryUpdateSchema = z.object({
     name: z.string().max(100).nonempty()
 });
