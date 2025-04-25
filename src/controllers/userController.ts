130dc90e4b5c72269245be0511d8c8a7a7d9e4f4
titouan-pichon <titouan.pichon@oclock.school>
2025-04-25 15:53:14 +0200

    Add user.schema.ts

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,18 +5,24 @@ export const userController = {
     async getUserDatas(req, res) {
         try {
             const id = req.user.id;
-
             const user = await User.findByPk(id, {
                 attributes: { exclude: ['password'] }
             });
-            
             if(!user) {
                 return res.status(404).json('Utilisateur non trouvé');
             }
-
             res.status(200).json(user);
         } catch (error) {
             res.status(500).json('Erreur interne du serveur');
+        }
+    },
+
+    async updateUserDatas(req, res) {
+        try {
+            const id = req.user.id;
+            const updatedDatas = req.body;
+            const user = await User.findByPk(id);
+
         }
     }
 }
