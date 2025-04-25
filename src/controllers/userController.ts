4cdb0cf5f84b8d9f261e32ccaec45208ea324189
titouan-pichon <titouan.pichon@oclock.school>
2025-04-25 15:16:45 +0200

    Start getUserDatas function

---
+++ src/controllers/userController.ts
@@ -0,0 +1,10 @@
+import { Sequelize } from "sequelize";
+import { User } from "../models/association.model.js";
+
+export const userController = {
+    async getUserDatas(req, res) {
+        try {
+            
+        }
+    }
+}
