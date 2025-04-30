f1f949f05b6cc29ca13290831af0e4236b1752b6
william <william.dodart@gmail.com>
2025-04-30 09:44:31 +0200

    Finalisaton of endpoints testing

--- src/router.ts
+++ src/router.ts
@@ -7,48 +7,45 @@ import { userController } from './controllers/userController.js';
 
 export const router = Router();
 
-//--------------------------------------TESTED ----------------------------------------------------------------
-
+//BOOKS
 router.get('/books', bookController.getAllBooks);
 router.get('/book/:id', bookController.getOneBookById); // Nodemon bugged with Regex expression -> to fix ?
 router.get('/random-books', bookController.getFiveRandomBooks);
 
+//ADMIN
 router.post('/admin/book', adminController.createBook);
 router
   .route('/admin/book/:id')
   .patch(adminController.editBook)
   .delete(adminController.deleteBook);
 
-router
-  .route('/library/:libraryId/book/:bookId')
-  .post(libraryController.addBookToLibrary)
-  .patch(libraryController.editBookStatus)
-  .delete(libraryController.deleteBook);
+//LIBRARIES
 router.get(
   '/user/:id/libraries',
   libraryController.getLibrariesWithoutBooksByUserId,
 );
-
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-router
-  .route('/user')
-  .get(userController.getUserDatas)
-  .patch(userController.updateUserDatas); // => Issue with updating only one field, password is not hashed.
-
-//-------------------------------------------------------------------------------------------------------------
-//--------------------------------------NOT TESTED ------------------------------------------------------------
-
 router.get(
   '/user/:id/libraries/books',
   libraryController.getLibrariesWithBooksByUserId,
 );
-
-router.post('/library', libraryController.createNewLibrary);
-
 router
   .route('/library/:id')
   .get(libraryController.getLibraryById)
   .patch(libraryController.updateLibraryName)
   .delete(libraryController.deleteLibrary);
+router
+  .route('/library/:libraryId/book/:bookId')
+  .post(libraryController.addBookToLibrary)
+  .patch(libraryController.editBookStatus)
+  .delete(libraryController.deleteBook);
+router.post('/library', libraryController.createNewLibrary);
+
+//AUTHENTIFICATION
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+
+//USER
+router
+  .route('/user')
+  .get(userController.getUserDatas)
+  .patch(userController.updateUserDatas); // => Issue with updating only one field, password is not hashed.
