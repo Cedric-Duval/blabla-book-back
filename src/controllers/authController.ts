c5f4c5b8597baa78793d49bdf2bf166cc447c513
william <william.dodart@gmail.com>
2025-05-07 12:15:37 +0200

    Implementing custom erros and wrapController for authController

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,51 +1,34 @@
-import bcrypt from 'bcrypt';
-import { z } from 'zod';
 import { BadRequestError, NotFoundError } from '../errors/customErrors.js';
 import { User } from '../models/association.model.js';
 import { createUser, loginUser } from '../schemas/auth.schema.js';
-import { checkPassword, createToken } from '../utils/authUtils.js';
+import {
+  checkPassword,
+  createToken,
+  hashPassword,
+} from '../utils/authUtils.js';
 
 export const authController = {
   async register(req, res) {
-    try {
-      const parsedData = createUser.parse(req.body);
+    const parsedData = createUser.parse(req.body);
 
-      const existingUser = await User.findOne({
-        where: { email: parsedData.email },
-      });
+    const existingUser = await User.findOne({
+      where: { email: parsedData.email },
+    });
 
-      if (existingUser) {
-        return res.status(400).json({
-          errors: [
-            {
-              field: 'email',
-              message: 'Cette adresse mail est déjà utilisée',
-            },
-          ],
-        });
-      }
-
-      const hashedPassword = await bcrypt.hash(parsedData.password, 10);
-      const newUser = await User.create({
-        name: parsedData.name,
-        firstname: parsedData.firstname,
-        email: parsedData.email,
-        password: hashedPassword,
-      });
-
-      res.status(201).json(newUser);
-    } catch (error) {
-      if (error instanceof z.ZodError) {
-        const zodErrors = error.errors.map((err) => ({
-          field: err.path[0],
-          message: err.message,
-        }));
-        return res.status(400).json({ errors: zodErrors });
-      }
-
-      console.error(error);
-      res.status(500).json({ error: 'Erreur interne du serveur' });
+    if (existingUser) {
+      throw new BadRequestError('Adresse mail déjà utilisée', 'email');
     }
+
+    const hashedPassword = await hashPassword(parsedData.password);
+
+    const newUser = await User.create({
+      name: parsedData.name,
+      firstname: parsedData.firstname,
+      email: parsedData.email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json(newUser);
   },
 
   async login(req, res) {
@@ -56,7 +39,7 @@ export const authController = {
     });
 
     if (!currentUser) {
-      throw new BadRequestError('Adress mail invalide', 'email');
+      throw new BadRequestError('Adresse mail invalide', 'email');
     }
 
     if (!(await checkPassword(parsedData.password, currentUser.password))) {
--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -14,3 +14,8 @@ export function createToken(currentId: number, currentEmail: string) {
   );
   return token;
 }
+
+export async function hashPassword(password: string) {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  return hashedPassword;
+}
