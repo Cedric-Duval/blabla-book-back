d036ecffab62df531d7b164a128330dba9c76cbb
william <william.dodart@gmail.com>
2025-05-12 13:59:13 +0200

    Adding Genres in endpoint response

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,5 @@
 import 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
-import { errorHandler } from './middlewares/errorHandler.js';
-import { router } from './router.js';
-
-const app = express();
-
-app.use(
-  cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
-  }),
-);
-
-app.use(express.json());
-
-app.use(router);
-
-app.use(errorHandler);
+import app from './app.js';
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
