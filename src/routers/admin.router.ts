e07a5ebeca39932d51433b2bb829268c809c0318
william <william.dodart@gmail.com>
2025-05-14 18:05:52 +0200

    Ajusting Security

--- src/routers/admin.router.ts
+++ src/routers/admin.router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { adminController } from '../controllers/adminController';
+import { authMiddleware } from '../middlewares/authMiddleware';
 import { wrapController } from '../middlewares/wrapController';
 
 export const adminRouter = Router();
 
-adminRouter.post('/admin/book', wrapController(adminController.createBook));
+adminRouter.post('/admin/book',authMiddleware.authorization, wrapController(adminController.createBook));
 adminRouter.route('/admin/book/:id')
-  .patch(wrapController(adminController.editBook))
-  .delete(wrapController(adminController.deleteBook));
+  .patch(authMiddleware.authorization, wrapController(adminController.editBook))
+  .delete(authMiddleware.authorization, wrapController(adminController.deleteBook));
--- src/routers/books.router.ts
+++ src/routers/books.router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { bookController } from '../controllers/bookController';
+import { authMiddleware } from '../middlewares/authMiddleware';
 import { wrapController } from '../middlewares/wrapController';
 
 export const booksRouter = Router();
 
-booksRouter.get('/books', wrapController(bookController.getAllBooks));
-booksRouter.get('/book/:id', wrapController(bookController.getOneBookById));
+booksRouter.get('/books',authMiddleware.authorization, wrapController(bookController.getAllBooks));
+booksRouter.get('/book/:id', authMiddleware.authorization, wrapController(bookController.getOneBookById));
 booksRouter.get('/random-books', wrapController(bookController.getFiveRandomBooks));
