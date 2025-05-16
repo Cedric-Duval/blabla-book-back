f762a069528c2ae7083e6282bc06c1968b36f8d1
titouan-pichon <titouan.pichon@oclock.school>
2025-05-16 23:10:06 +0200

    Created reviewController

---
+++ src/controllers/reviewController.ts
@@ -0,0 +1,68 @@
+import { Request, Response } from 'express';
+import { Review, Book, User } from '../models/association.model';
+import { checkFoundBook } from '../errors/checkErros';
+
+export const reviewController = {
+  async getReviewsForBook(req: Request, res: Response) {
+    const { bookId } = req.params;
+
+    const book = await Book.findByPk(bookId);
+    checkFoundBook(book);
+
+    const reviews = await Review.findAll({
+      where: { book_id: bookId },
+      include: {
+        model: User,
+        attributes: ['id', 'firstname', 'name'],
+      },
+      order: [['createdAt', 'DESC']],
+    });
+
+    res.status(200).json(reviews);
+  },
+
+  async createReview(req: Request, res: Response) {
+    const { bookId } = req.params;
+    const { content, rating } = req.body;
+
+    const book = await Book.findByPk(bookId);
+    checkFoundBook(book);
+
+    const review = await Review.create({
+      content,
+      rating,
+      user_id: req.user.id,
+      book_id: bookId,
+    });
+
+    res.status(201).json(review);
+  },
+
+  async updateReview(req: Request, res: Response) {
+    const { id } = req.params;
+    const { content, rating } = req.body;
+
+    const review = await Review.findByPk(id);
+    if (!review || review.user_id !== req.user.id) {
+      return res.status(403).json({ error: 'Cette review n\'existe pas ou alors vous n\'êtes pas autorisé à la modifier.' });
+    }
+
+    review.content = content ?? review.content;
+    review.rating = rating ?? review.rating;
+    await review.save();
+
+    res.status(200).json(review);
+  },
+
+  async deleteReview(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const review = await Review.findByPk(id);
+    if (!review || review.user_id !== req.user.id) {
+      return res.status(403).json({ error: 'Cette review n\'existe pas ou alors vous n\'êtes pas autorisé à la modifier.' });
+    }
+
+    await review.destroy();
+    res.status(204).send();
+  },
+};
--- src/routers/review.router.ts
+++ src/routers/review.router.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { reviewController } from '../controllers/reviewController';
+import { reviewController } from '../controllers/reviewController.ts';
 import { authMiddleware } from '../middlewares/authMiddleware';
 import { wrapController } from '../middlewares/wrapController';
 
 export const reviewRouter = Router();
 
-
+// Authenticated : Get all the reviews from a book and create a review
 reviewRouter
   .route('/book/:bookId/reviews')
   .get(authMiddleware.authorization, wrapController(reviewController.getReviewsForBook))
